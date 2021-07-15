ef51de4d67a82c5ca64c57f43912dbbc0f85aad1
My Name <alex971568906558@gmail.com>
2021-07-15 17:01:15 +0300

    1

---
+++ run.py
@@ -0,0 +1,34 @@
+from aiogram import types, executor, Dispatcher, Bot
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+TOKEN = "1854908281:AAFDQoSyv7JSWcR7FvVYCfQ6X-zFy_YoBrU"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def begin (message: types.Message):
+    markup = InlineKeyboardMarkup()
+    but_1 = InlineKeyboardButton("Как дила?", callback_data="but_1")
+    markup.add(but_1)
+    await bot.send_message(message.chat.id, 'Привет, Лоло!', reply_markup=markup)
+
+
+@dp.callback_query_handler(lambda c: c.data == "but_1")
+async def button_reaction(call: types.callback_query):
+
+    await bot.answer_callback_query(call.id)
+    await bot.send_message(call.message.chat.id, "Дела мои хорошуганские!")
+
+
+@dp.message_handler(content_types=['text'])
+async def text (message: types.Message):
+    if message.text.lower() == 'пока':
+        await bot.send_message(message.chat.id, 'пакеда!')
+        #await message.reply('Пакеда!')
+
+
+
+
+executor.start_polling(dp)
